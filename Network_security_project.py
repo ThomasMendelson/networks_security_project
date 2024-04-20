28483e03cb9c582c8d07396e2e8ca8eb118293e0
Yehonatan Arama <yoni1arama@gmail.com>
2024-04-20 23:54:59 +0300

    many Changes. yoni 20/04/2024 23:55

--- Network_security_project.py
+++ Network_security_project.py
@@ -7,135 +7,141 @@ Thomas Mendelson 209400654
 import math
 import random
 import sys
+import numpy as np
+
+
+def DefaultCostFunc(self, SigXr):
+    return math.exp(SigXr)
 
 class Link:
     capacity = None
     users = set()
 
-    def __init__(self,firstUser,secondUser):
+    def __init__(self, firstUser, secondUser):
         self.users.add(firstUser)
         self.users.add(secondUser)
         self.capacity = 1
 
-
-    def Contains (self,friend):
+    def Contains(self, friend):
         return friend in self.users
 
-    def SetCapacity(self , n):
+    def SetCapacity(self, n):
         self.capacity = n
+
+
 class User:
     x_pos = None
     y_pos = None
     links = set()
-    #power = None
-    #bw = None
     Xr = None
 
-    def __init__(self,M):
+    def __init__(self, M):
         radius = random.uniform(0, M)
         theta = random.uniform(0, 2 * math.pi)
         self.x_pos = radius * math.cos(theta)
         self.y_pos = radius * math.sin(theta)
         self.Xr = 1
 
-    def Dist (self , friend):
-        return math.sqrt((self.x_pos - friend.x_pos)*2 + (self.y_pos - friend.y_pos)*2)
-    def HasLink (self , friend):
+    def __init__(self,x_pos,y_pos):
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.Xr = 1
+
+    def Dist(self, friend):
+        return math.sqrt((self.x_pos - friend.x_pos) * 2 + (self.y_pos - friend.y_pos) * 2)
+
+    def HasLink(self, friend):
         for link in self.links:
             if link.Contains(friend):
                 return True
         return False
+
     def AddLink(self, friend):
-        link = Link(self,friend)
+        link = Link(self, friend)
         self.links.add(link)
         friend.links.add(link)
         return link
 
-    def SetXr(self,xr):
+    def SetXr(self, xr):
         self.Xr = xr
 
     def GetXr(self):
         return self.Xr
-
-    #def SetPower (self,power):
-    #    self.power = power
-
-    #def SetBW (self,bw):
-    #    self.bw = bw
 
 class Graph:
     N = None
     M = None
     r = None
     alpha = None
-    #F = None
     users = []
     links = set()
-    #hI = []
-    #flows =[] #Two dimensional list each line is a Flow l[0] is source l[1] is destination l[2] is data
+    NumOfLinks = None
+    stepSize = None
+    maxIterSteps = None
+    costFunc = None
+    routes = []         #Two dimensional array - the rows are source, the collomns are destination and the cells are list of links on the route
 
-    #def __init__(self, N,M,r,F,func_power,func_BW,func_hI):
-    def __init__(self, N,M,r,alpha):
+    #TODO routes are not implemented yet in the random constructor
+    def __init__(self, N, M, r, alpha):
         self.N = N
         self.M = M
         self.r = r
         self.alpha = alpha
-        self.CreateGraph()
-        #self.SetPowerToGraph(func_power)
-        #self.SetBWToGraph(func_BW)
-        #self.SethIToGraph(func_hI)
-        #self.CreateFlows()
+        self.CreateRandomGraph()
+        self.NumOfLinks = self.GetNumLinks()
 
-    def CreateGraph(self):
+    def __init__(self, links , users , routes, alpha , costFunc = DefaultCostFunc , stepSize = lambda user: 1e-3 , maxIterSteps = 2000):
+        self.N = len(users)
+        self.users = users
+        self.links = links
+        self.alpha = alpha
+        self.routes = routes
+        self.NumOfLinks = len(links)
+        self.stepSize = stepSize
+        self.maxIterSteps = maxIterSteps
+        self.costFunc = costFunc
+
+    def CreateRandomGraph(self):
         for i in range(self.N):
             self.users.append(User(self.M))
         for user in self.users:
             for friend in self.users:
                 if (user == friend):
                     continue
-                if user.Dist(friend)<self.r:
-                    if not(user.HasLink(friend)):
+                if user.Dist(friend) < self.r:
+                    if not (user.HasLink(friend)):
                         self.links.add(user.AddLink(friend))
 
     def GetNumLinks(self):
         return len(self.links)
 
-    #def SetAllCapacityTo(self, n):
-    #    for link in self.links:
-    #        link.SetCapacity(n)
+    def Ur(self, user):
+        return (user.GetXr()(1 - self.alpha)) / (1 - self.alpha)
 
-    def Ur(self , user):
-        return (user.GetXr()(1-self.alpha))/(1-self.alpha)
+    def DevUr(self, user):
+        return 1/(user.GetXr()**self.alpha)
 
-    #def SetPowerToGraph(self, func):
-    #    func(self)
+    def Kr(self, user):
+        return self.stepSize(user)
 
-    #def SetBWToGraph(self, func):
-    #    func(self)
+    def PrimalIterStep(self,xr,dUr):
+        SigXr = 0
+        for user in self.users:
+            #TODO need to go over the links in the user route, and for each link to sum up the Xr of all users that uses this link
+            #TODO problem  - we need to fix this function and also the one that calls it.
+        return
+    def runPrimal(self):
+        XrsForToPlot = np.zeros((len(self.users),1))
+        for i in len(self.users):
+            XrsForToPlot[i].insert(0,self.users[i].GetXr())
+        for i in range(self.maxIterSteps):
+            randIndex = random.randint(0,len(self.users)-1)
+            chosenUser = self.users[randIndex]
+            chosenUser.SetXr(self.PrimalIterStep(chosenUser.GetXr() , self.DevUr(chosenUser)))
+            XrsForToPlot[randIndex].append(chosenUser.GetXr())
 
-    #def SethIToGraph(self, func):
-    #    hI = func(self)
-
-    #def CreateFlows(self):
-    #    for i in range(self.F):
-    #        self.flows[i][0] = random.uniform(0,len(self.users))
-    #        self.flows[i][1] = random.uniform(0,len(self.users)-1)
-    #        if self.flows[i][0] == self.flows[i][1]:
-    #            self.flows[i][1] = len(self.users)-1
-    #        self.flows[i][2] = random.uniform(0,1e6)
 
 
-
-
-#def func_hIZeros(g):
-#    for i in range(len(g.users)):
-#        g.hI[i] = []
-#        for j in range(len(g.users)):
-#            g.hI[i][j] = 0
-
-def Run_question2(N , M , r , alpha):
-    g = Graph(N,M,r)
-    NumOfLinks = g.GetNumLinks()
 
 
 
@@ -144,11 +150,13 @@ def Run_question2(N , M , r , alpha):
 '''
 Arguments for program:
 question - the question that we want to test
-N - number of users
-M - radius of the simulation
-r - radius around users for links
-alpha - for alpha fairness
+-N - number of users
+-M - radius of the simulation
+-r - radius around users for links
+-alpha - for alpha fairness
 '''
+
+
 
 def main():
     args = sys.argv[1:]
@@ -157,17 +165,8 @@ def main():
     M = args[2]
     r = args[3]
     alpha = args[4]
-    if (question==2):
-        Run_question2(N , M , r , alpha)
-
-
-
-
-
 
 
 
 if __name__ == "__main__":
     main()
-
-#yoni
