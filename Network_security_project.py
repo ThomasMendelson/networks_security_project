07f3b4e5149d7ba4dbd60e480b9cddb92d59fdc5
Yehonatan Arama <yoni1arama@gmail.com>
2024-04-21 09:40:11 +0300

    21/04/2024 09:40 finish Primal?

--- Network_security_project.py
+++ Network_security_project.py
@@ -10,63 +10,80 @@ import sys
 import numpy as np
 
 
-def DefaultCostFunc(self, SigXr):
-    return math.exp(SigXr)
+def DefaultCostFunc(self, sigXr):
+    return math.exp(sigXr)
+
 
 class Link:
     capacity = None
+    connectedTo = set()
     users = set()
 
-    def __init__(self, firstUser, secondUser):
-        self.users.add(firstUser)
-        self.users.add(secondUser)
-        self.capacity = 1
+    def __init__(self, firstUser, secondUser, capacity):
+        self.connectedTo.add(firstUser)
+        self.connectedTo.add(secondUser)
+        self.capacity = capacity
 
-    def Contains(self, friend):
-        return friend in self.users
+    def Connected(self, user):
+        return user in self.connectedTo
 
     def SetCapacity(self, n):
         self.capacity = n
+
+    def GetCapacity(self):
+        return self.capacity
+
+    def Adduser(self, user):
+        self.users.add(user)
+
+    def RemoveUser(self, user):
+        if user in self.users:
+            self.users.remove(user)
 
 
 class User:
     x_pos = None
     y_pos = None
-    links = set()
+    connectedLinks = set()
+    linksInRoutes = set()
     Xr = None
 
-    def __init__(self, M):
-        radius = random.uniform(0, M)
-        theta = random.uniform(0, 2 * math.pi)
-        self.x_pos = radius * math.cos(theta)
-        self.y_pos = radius * math.sin(theta)
-        self.Xr = 1
+    #def __init__(self, M):
+    #    radius = random.uniform(0, M)
+    #    theta = random.uniform(0, 2 * math.pi)
+    #    self.x_pos = radius * math.cos(theta)
+    #    self.y_pos = radius * math.sin(theta)
+    #    self.Xr = 1
 
-    def __init__(self,x_pos,y_pos):
-        self.x_pos = x_pos
-        self.y_pos = y_pos
+    def __init__(self, connectedLinks, linksInRoutes):
+        self.connectedLinks = connectedLinks
+        self.linksInRoutes = linksInRoutes
         self.Xr = 1
 
     def Dist(self, friend):
         return math.sqrt((self.x_pos - friend.x_pos) * 2 + (self.y_pos - friend.y_pos) * 2)
 
-    def HasLink(self, friend):
-        for link in self.links:
-            if link.Contains(friend):
+    def IsConnectedTo(self, friend):
+        for link in self.connectedLinks:
+            if link.Connected(friend):
                 return True
         return False
 
-    def AddLink(self, friend):
-        link = Link(self, friend)
-        self.links.add(link)
-        friend.links.add(link)
+    def AddConnectedLink(self, friend, capacity):
+        link = Link(self, friend, capacity)
+        self.connectedLinks.add(link)
+        friend.connectedLinks.add(link)
         return link
+
+    def AddlinksInRoutes(self, link):
+        self.linksInRoutes.add(link)
 
     def SetXr(self, xr):
         self.Xr = xr
 
     def GetXr(self):
         return self.Xr
+
 
 class Graph:
     N = None
@@ -79,70 +96,72 @@ class Graph:
     stepSize = None
     maxIterSteps = None
     costFunc = None
-    routes = []         #Two dimensional array - the rows are source, the collomns are destination and the cells are list of links on the route
 
-    #TODO routes are not implemented yet in the random constructor
-    def __init__(self, N, M, r, alpha):
-        self.N = N
-        self.M = M
-        self.r = r
-        self.alpha = alpha
-        self.CreateRandomGraph()
-        self.NumOfLinks = self.GetNumLinks()
+    # routes = []         #Two dimensional array - the rows are source, the collomns are destination and the cells are list of links on the route
 
-    def __init__(self, links , users , routes, alpha , costFunc = DefaultCostFunc , stepSize = lambda user: 1e-3 , maxIterSteps = 2000):
+    # TODO routes are not implemented yet in the random constructor
+    # def __init__(self, N, M, r, alpha):
+    #    self.N = N
+    #    self.M = M
+    #    self.r = r
+    #    self.alpha = alpha
+    #    self.CreateRandomGraph()
+    #    self.NumOfLinks = self.GetNumLinks()
+
+    def __init__(self, links, users, alpha, costFunc=DefaultCostFunc, stepSize=lambda user: 1e-3, maxIterSteps=2000):
         self.N = len(users)
         self.users = users
         self.links = links
         self.alpha = alpha
-        self.routes = routes
         self.NumOfLinks = len(links)
         self.stepSize = stepSize
         self.maxIterSteps = maxIterSteps
         self.costFunc = costFunc
 
-    def CreateRandomGraph(self):
-        for i in range(self.N):
-            self.users.append(User(self.M))
-        for user in self.users:
-            for friend in self.users:
-                if (user == friend):
-                    continue
-                if user.Dist(friend) < self.r:
-                    if not (user.HasLink(friend)):
-                        self.links.add(user.AddLink(friend))
+    # def CreateRandomGraph(self):
+    #    for i in range(self.N):
+    #        self.users.append(User(self.M))
+    #    for user in self.users:
+    #       for friend in self.users:
+    #            if (user == friend):
+    #                continue
+    #            if user.Dist(friend) < self.r:
+    #                if not (user.HasLink(friend)):
+    #                    self.links.add(user.AddLink(friend))
 
     def GetNumLinks(self):
-        return len(self.links)
+        return self.NumOfLinks
 
     def Ur(self, user):
         return (user.GetXr()(1 - self.alpha)) / (1 - self.alpha)
 
     def DevUr(self, user):
-        return 1/(user.GetXr()**self.alpha)
+        return 1 / (user.GetXr() ** self.alpha)
 
     def Kr(self, user):
         return self.stepSize(user)
 
-    def PrimalIterStep(self,xr,dUr):
-        SigXr = 0
-        for user in self.users:
-            #TODO need to go over the links in the user route, and for each link to sum up the Xr of all users that uses this link
-            #TODO problem  - we need to fix this function and also the one that calls it.
-        return
+    def PrimalIterStep(self, chosenUser):
+        dUr = self.DevUr(chosenUser)
+        kr = self.Kr(chosenUser)
+        SigCost = 0
+        for l in chosenUser.linksInRoutes:
+            SigXr = 0
+            for user in l.users:
+                SigXr += user.GetXr()
+            SigCost += self.costFunc(DefaultCostFunc, SigXr)
+        return kr * (dUr - SigCost)
+
     def runPrimal(self):
-        XrsForToPlot = np.zeros((len(self.users),1))
-        for i in len(self.users):
-            XrsForToPlot[i].insert(0,self.users[i].GetXr())
+        XrsToPlot = []
+        for i in range(len(self.users)):
+            XrsToPlot.insert(i, [])
+            XrsToPlot[i].insert(0, self.users[i].GetXr())
         for i in range(self.maxIterSteps):
-            randIndex = random.randint(0,len(self.users)-1)
+            randIndex = random.randint(0, len(self.users) - 1)
             chosenUser = self.users[randIndex]
-            chosenUser.SetXr(self.PrimalIterStep(chosenUser.GetXr() , self.DevUr(chosenUser)))
-            XrsForToPlot[randIndex].append(chosenUser.GetXr())
-
-
-
-
+            chosenUser.SetXr(self.PrimalIterStep(chosenUser))
+            XrsToPlot[randIndex].append(chosenUser.GetXr())
 
 
 # Tamplate for run:
@@ -157,7 +176,6 @@ question - the question that we want to test
 '''
 
 
-
 def main():
     args = sys.argv[1:]
     question = args[0]
@@ -165,7 +183,6 @@ def main():
     M = args[2]
     r = args[3]
     alpha = args[4]
-
 
 
 if __name__ == "__main__":
